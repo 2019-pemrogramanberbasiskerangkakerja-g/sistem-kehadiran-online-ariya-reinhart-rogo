d57caad061066a279c72af3d945e315a1eed37d6
ariya <ariyadevanto@gmail.com>
2019-04-28 22:48:10 +0700

    session

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var app = express();
+
+app.use(cookieParser());
+app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }}))
 
 var mysql = require('mysql')
 var connection = mysql.createConnection({
@@ -9,33 +15,43 @@ var connection = mysql.createConnection({
   database: 'pbkk'
 })
 
-connection.connect()
- 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 router.get('/login', function(req, res, next) {
+  req.session.page_views = 1;
   res.render('login', { title: 'Login' });
 });
 router.get('/register', function(req, res, next) {
   res.render('register', { title: 'Register' });
 });
-router.post('/login', function(req, res, next) {
+router.get('/home', function(req, res, next) {
+    if(req.session) {
+      // var nrp = req.session.nrp;
+      res.render('home');
+    }
+    else {
+      res.redirect('/login');
+    }
+  // res.send("hai")
+});
+router.post('/loginoke', function(req, res, next) {
   var nrp = req.body.nrp;
   var pass = req.body.pass;
+  sess = req.session;
   // console.log(pass);
   var sql = 'SELECT * FROM akun WHERE nrp = ? and password = ?';
   connection.query(sql, [nrp,pass], function (err, result) {
-    if (err) throw err;
-    else if (!result.length) {                                                   
-      res.render('login', { title: 'Login' });
-    }
-    else{
-      res.render('home', { title: 'Login' });
-    }
-    console.log(result);
+    if (result.length > 0) {
+      req.session.loggedin = true;
+      req.session.username = username;
+      res.redirect('/home');
+    } else {
+      res.send('Incorrect Username and/or Password!');
+    }			
+    res.end();
   });
- 
 });
 module.exports = router;
