78d92a7fadb3bd5c25b6348c987f31aa1520059a
Reinhart Caesar <sgs3.rc@gmail.com>
2019-05-05 16:00:10 +0700

    Login API

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.set('view engine', 'ejs')
 
 app.get('/', checkSignIn, (req, res) => {
     console.log(Users)
-    res.render('index.ejs', { username: req.session.user.username })
+    res.render('index.ejs', { nrp: req.session.user.nrp })
 })
 
 app.get('/login', (req, res) => {
@@ -62,31 +62,29 @@ app.post('/register', (req, res) => {
 })
 
 app.post('/login', (req, res) => {
-    let username = req.body['username']
+    let nrp = req.body['nrp']
     let password = req.body['password']
 
-    let user = userRealm.objects('User').filtered(
-        'username = "' + username + '"' + ' AND ' + 'password = "' + password + '"'
-    )
-
-    if (user.length == 0) {
-        res.send("Login failed, user not found")
-    }
-    else {
-        Users.filter((user) => {
-            if (user.id === username) {
-                res.send("This user already logged in");
-            }
+    agent.post('localhost:3001/user')
+        .send({
+            nrp: nrp,
+            password: password
         })
-
-        let newUser = { username: username, password: password };
-
-        Users.push(newUser);
-
-        req.session.user = newUser;
-
-        res.redirect('/')
-    }
+        .then(
+            (response) => {
+                if (response.status == 200) {
+                    let newUser = { nrp: nrp, password: password };
+                    Users.push(newUser);
+                    req.session.user = newUser;
+                    res.redirect('/')
+                }
+            }
+        )
+        .catch(
+            (err) => {
+                console.log(err)
+            }
+        )
 })
 
 app.get('/delete', (req, res) => {
